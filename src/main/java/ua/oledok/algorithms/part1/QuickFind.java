1d66426220c8747cce7ba0ac25c85ba37089fd5c
Oleh Dokuka <shadowgun@i.ua>
2017-03-01 18:23:06 +0200

    implements common interface, provide additional common methods implementations

--- src/main/java/ua/oledok/algorithms/part1/QuickFind.java
+++ src/main/java/ua/oledok/algorithms/part1/QuickFind.java
@@ -1,15 +1,28 @@
 package ua.oledok.algorithms.part1;
 
-public class QuickFind {
+public class QuickFind implements DynamicConnectivity {
     private final int[] ids;
+    private int size;
 
     public QuickFind(int[] values) {
-        ids = new int[values.length];
-        System.arraycopy(values, 0, ids, 0, values.length);
+        size = values.length;
+        ids = new int[size];
+
+        System.arraycopy(values, 0, ids, 0, size);
+    }
+
+    @Override
+    public int size() {
+        return size;
     }
 
     public boolean isConnected(int q, int p) {
         return ids[q] == ids[p];
+    }
+
+    @Override
+    public int root(int value) {
+        return ids[value];
     }
 
     public void union(int q, int p) {
--- src/main/java/ua/oledok/algorithms/part1/QuickUnion.java
+++ src/main/java/ua/oledok/algorithms/part1/QuickUnion.java
@@ -1,19 +1,32 @@
 package ua.oledok.algorithms.part1;
 
 
-public class QuickUnion {
+public class QuickUnion implements DynamicConnectivity {
     private final int[] ids;
+    private int size;
 
     public QuickUnion(int[] values) {
-        ids = new int[values.length];
-        System.arraycopy(values, 0, ids, 0, values.length);
+        size = values.length;
+        ids = new int[size];
+
+        System.arraycopy(values, 0, ids, 0, size);
     }
 
     public void union(int q, int p) {
         int qRoot = root(q);
         int pRoot = root(p);
 
+        if (qRoot == pRoot) {
+            return;
+        }
+
         ids[qRoot] = pRoot;
+        size--;
+    }
+
+    @Override
+    public int size() {
+        return size;
     }
 
     public boolean isConnected(int q, int p) {
